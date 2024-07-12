b0f583a815cf0e14b5c99f4bf51381a4d71e2e68
CppCXY <812125110@qq.com>
2024-07-12 20:50:26 +0800

    Add RenameParams and RenameHandlerBase classes for rename functionality

---
+++ LanguageServer.Framework/Protocol/Message/Rename/RenameParams.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using EmmyLua.LanguageServer.Framework.Protocol.Message.Interface;
+using EmmyLua.LanguageServer.Framework.Protocol.Model.TextDocument;
+
+namespace EmmyLua.LanguageServer.Framework.Protocol.Message.Rename;
+
+public class RenameParams : TextDocumentPositionParams, IWorkDoneProgressParams
+{
+    [JsonPropertyName("workDoneToken")]
+    public string? WorkDoneToken { get; set; }
+
+    /**
+     * The new name of the symbol. If the given name is not valid, the
+     * request must return a [ResponseError](#ResponseError) with an
+     * appropriate message set.
+     */
+    [JsonPropertyName("newName")]
+    public string NewName { get; set; } = null!;
+}
---
+++ LanguageServer.Framework/Server/Handler/RenameHandlerBase.cs
@@ -0,0 +1,17 @@
+﻿using EmmyLua.LanguageServer.Framework.Protocol.Capabilities.Client.ClientCapabilities;
+using EmmyLua.LanguageServer.Framework.Protocol.Capabilities.Server;
+using EmmyLua.LanguageServer.Framework.Protocol.Message.Rename;
+using EmmyLua.LanguageServer.Framework.Protocol.Model;
+
+namespace EmmyLua.LanguageServer.Framework.Server.Handler;
+
+public abstract class RenameHandlerBase : IJsonHandler
+{
+    protected abstract Task<WorkspaceEdit?> Handle(RenameParams request, CancellationToken token);
+
+    public void RegisterHandler(LanguageServer server)
+    {
+    }
+
+    public abstract void RegisterCapability(ServerCapabilities serverCapabilities, ClientCapabilities clientCapabilities);
+}
