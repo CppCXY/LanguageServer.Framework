2adc2ada2291479c514a757bac5a8ef47a5bcdb1
CppCXY <812125110@qq.com>
2024-07-15 11:05:12 +0800

    Refactor DocumentUri.cs to improve file path handling

--- LanguageServer.Framework/Protocol/Model/DocumentUri.cs
+++ LanguageServer.Framework/Protocol/Model/DocumentUri.cs
@@ -8,6 +8,26 @@ public record struct DocumentUri(Uri Uri)
 {
     public Uri Uri { get; } = Uri;
 
+    public string UnescapeUri => Uri.UnescapeDataString(Uri.AbsoluteUri);
+
+    public string FileSystemPath
+    {
+        get
+        {
+            var filePath = Uri.UnescapeDataString(Uri.AbsolutePath);
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                if (filePath.StartsWith("/"))
+                {
+                    filePath = filePath.TrimStart('/');
+                }
+                filePath = filePath.Replace("/", "\\");
+            }
+
+            return filePath;
+        }
+    }
+
     public static implicit operator DocumentUri(Uri uri) => new DocumentUri(uri);
 
     public static implicit operator DocumentUri(string uri) => new DocumentUri(new Uri(uri));
