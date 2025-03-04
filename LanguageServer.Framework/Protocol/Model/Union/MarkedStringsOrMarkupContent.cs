fff672c5fc65b1b98d75190f7a7f5d0d46b21bab
望尘空忧 <wckfxxjstdwckyxphdhqxg@outlook.com>
2025-03-04 18:14:12 +0800

    · 使用一些奇技淫巧模拟Union让MarkedStringsOrMarkupContent的设计更加严谨。

--- LanguageServer.Framework/Protocol/Model/Union/MarkedStringsOrMarkupContent.cs
+++ LanguageServer.Framework/Protocol/Model/Union/MarkedStringsOrMarkupContent.cs
@@ -7,10 +7,11 @@ using EmmyLua.LanguageServer.Framework.Protocol.Supplement;
 namespace EmmyLua.LanguageServer.Framework.Protocol.Union;
 
 [JsonConverter(typeof(MarkedStringsOrMarkupContentJsonConverter))]
-public record MarkedStringsOrMarkupContent
+public abstract record MarkedStringsOrMarkupContent
 {
-    public List<MarkedString>? MarkedStrings { get; init; }
-    public MarkupContent? MarkupContent { get; init; }
+    public sealed record InternalMarkedStrings(List<MarkedString>? MarkedStrings) : MarkedStringsOrMarkupContent;
+
+    public sealed record InternalMarkupContent(MarkupContent? Content) : MarkedStringsOrMarkupContent;
 }
 
 public class MarkedStringsOrMarkupContentJsonConverter : JsonConverter<MarkedStringsOrMarkupContent>
@@ -18,49 +19,35 @@ public class MarkedStringsOrMarkupContentJsonConverter : JsonConverter<MarkedStr
     public override MarkedStringsOrMarkupContent? Read(ref Utf8JsonReader reader, Type typeToConvert,
         JsonSerializerOptions options)
     {
+        var jsonNode = JsonNode.Parse(ref reader);
         if (reader.TokenType == JsonTokenType.StartObject)
         {
-            var jsonObj = JsonObject.Parse(ref reader);
-            if (jsonObj?["language"] is not null)
-            {
-                var obj = jsonObj.Deserialize<MarkedString>(options);
-                return new()
-                {
-                    MarkedStrings = obj is not null ? [obj] : null
-                };
-            }
+            if (jsonNode?["language"] is not null && jsonNode.Deserialize<MarkedString>(options) is { } markedString)
+                return new MarkedStringsOrMarkupContent.InternalMarkedStrings([markedString]);
 
-            return new()
-            {
-                MarkupContent = jsonObj.Deserialize<MarkupContent>(options)
-            };
+            return new MarkedStringsOrMarkupContent.InternalMarkupContent(jsonNode.Deserialize<MarkupContent>());
         }
 
         if (reader.TokenType == JsonTokenType.StartArray)
-        {
-            var jsonArray = JsonArray.Parse(ref reader);
-            return new MarkedStringsOrMarkupContent()
-            {
-                MarkedStrings = jsonArray.Deserialize<List<MarkedString>>(options)
-            };
-        }
+
+            return new MarkedStringsOrMarkupContent.InternalMarkedStrings(jsonNode.Deserialize<List<MarkedString>>());
+
 
         if (reader.TokenType == JsonTokenType.String)
         {
-            return new()
-            {
-                MarkedStrings = [new MarkedString() { Value = reader.GetString() ?? string.Empty }]
-            };
+            return new MarkedStringsOrMarkupContent.InternalMarkedStrings([
+                new MarkedString() { Value = reader.GetString() ?? string.Empty }
+            ]);
         }
 
-        return new();
+        return null;
     }
 
     public override void Write(Utf8JsonWriter writer, MarkedStringsOrMarkupContent value, JsonSerializerOptions options)
     {
-        if (value.MarkedStrings is not null && value.MarkupContent is null)
-            writer.WriteRawValue(JsonSerializer.Serialize(value.MarkedStrings, options));
-        if (value.MarkupContent is not null && value.MarkedStrings is null)
-            writer.WriteRawValue(JsonSerializer.Serialize(value.MarkupContent, options));
+        if (value is MarkedStringsOrMarkupContent.InternalMarkedStrings markedStrings)
+            writer.WriteRawValue(JsonSerializer.Serialize(markedStrings.MarkedStrings, options));
+        if (value is MarkedStringsOrMarkupContent.InternalMarkupContent markedContent)
+            writer.WriteRawValue(JsonSerializer.Serialize(markedContent.Content, options));
     }
 }
